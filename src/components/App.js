7ecef56ea1e32cd67e45d7da65d861de01f0f71e
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-04-18 14:54:52 +0300

    Started assignment

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import ProjectList from "./ProjectList";
 import user from "../data/user";
 
 function App() {
+  
   return (
     <div>
       <NavBar />
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,12 +3,16 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{/* render ProjectItem components here */}
+      
+      </div>
     </div>
   );
-}
+  
+  }
 
 export default ProjectList;
